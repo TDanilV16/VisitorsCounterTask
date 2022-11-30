f22f51dc99fec1b9f5794ff2de82ce8e097f1400
Danil_Titarenko <danil.titarenko@gmail.com>
2022-11-30 22:59:22 +0500

    Added more statistics, updated main

--- main.py
+++ main.py
@@ -23,5 +23,27 @@ async def root(request: Request):
         today_visitors = statistics_counter.count_for_today()
         unique_visitors = statistics_counter.count_unique()
 
+        unique_visitors_for_today = \
+            statistics_counter.count_unique_for_today()
+        total_visits_for_month = \
+            statistics_counter.count_for_month()
+        total_visits_for_year = \
+            statistics_counter.count_for_year()
+        unique_visitors_for_this_month = \
+            statistics_counter.count_unique_for_month()
+        unique_visitors_for_this_year = \
+            statistics_counter.count_unique_for_this_year()
+
     print(f"New visitor: {client_host}")
-    return f"""Total visitors: {total_visits} For today {today}: {today_visitors} Unique visitors: {unique_visitors}"""
+    return f"""
+    Total visitors: {total_visits} 
+    Overall Unique visitors: {unique_visitors} 
+    Unique visitors for today: {unique_visitors_for_today}  
+    Unique visitors for this month: {unique_visitors_for_this_month} 
+    Unique visitors for this year: {unique_visitors_for_this_year} 
+    All visitors for today {today}: {today_visitors} 
+    All visitors for this month: {total_visits_for_month} 
+    All visitors for this year: {total_visits_for_year} 
+    """
+
+# uvicorn main:app --host 192.168.1.60  --port 80
--- statistics_counter.py
+++ statistics_counter.py
@@ -7,7 +7,8 @@ def write_in_table(entities):
 
     cursor = db.cursor()
 
-    cursor.execute("INSERT INTO Visitors VALUES(?, ?)", entities)
+    cursor\
+        .execute("INSERT INTO Visitors VALUES(?, ?)", entities)
 
     db.commit()
 
@@ -28,9 +29,36 @@ def count_for_today():
     today = dt.date.today().strftime("%d/%m/%y")
     db = sql.connect("data.sqlite")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM Visitors WHERE date = ?;", (today,))
+    cursor\
+        .execute("SELECT * FROM Visitors WHERE date = ?;", (today,))
     count = len(cursor.fetchall())
     db.close()
+
+    return count
+
+
+def count_for_month():
+    today = dt.date.today().strftime("%d/%m/%y")
+    month = today.split('/')[1]
+    db = sql.connect("data.sqlite")
+    cursor = db.cursor()
+    string = f'/{month}/'
+    cursor\
+        .execute("SELECT * FROM Visitors WHERE instr(date, ?)", (string, ))
+    count = len(cursor.fetchall())
+
+    return count
+
+
+def count_for_year():
+    today = dt.date.today().strftime("%d/%m/%y")
+    year = today.split('/')[2]
+    db = sql.connect("data.sqlite")
+    cursor = db.cursor()
+    string = f'/{year}'
+    cursor\
+        .execute("SELECT * FROM Visitors WHERE instr(date, ?)", (string,))
+    count = len(cursor.fetchall())
 
     return count
 
@@ -38,8 +66,47 @@ def count_for_today():
 def count_unique():
     db = sql.connect("data.sqlite")
     cursor = db.cursor()
-    cursor.execute("SELECT DISTINCT ip FROM Visitors")
+    cursor\
+        .execute("SELECT DISTINCT ip FROM Visitors")
     count = len(cursor.fetchall())
     db.close()
+
+    return count
+
+
+def count_unique_for_today():
+    today = dt.date.today().strftime("%d/%m/%y")
+    db = sql.connect("data.sqlite")
+    cursor = db.cursor()
+    cursor\
+        .execute("SELECT DISTINCT ip FROM Visitors WHERE date = ?;", (today,))
+    count = len(cursor.fetchall())
+    db.close()
+
+    return count
+
+
+def count_unique_for_month():
+    today = dt.date.today().strftime("%d/%m/%y")
+    db = sql.connect("data.sqlite")
+    cursor = db.cursor()
+    month = today.split('/')[1]
+    string = f'/{month}/'
+    cursor\
+        .execute("SELECT DISTINCT ip FROM Visitors WHERE instr(date, ?)", (string,))
+    count = len(cursor.fetchall())
+
+    return count
+
+
+def count_unique_for_this_year():
+    today = dt.date.today().strftime("%d/%m/%y")
+    db = sql.connect("data.sqlite")
+    cursor = db.cursor()
+    year = today.split('/')[2]
+    string = f'/{year}'
+    cursor\
+        .execute("SELECT DISTINCT ip FROM Visitors WHERE instr(date, ?)", (string,))
+    count = len(cursor.fetchall())
 
     return count
